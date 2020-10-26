d68e6a441aaaebab1fa36916132698d6a04db02c
Amirhossein Andohkosh <2172470-amirH.A@users.noreply.gitlab.com>
2020-10-26 14:43:04 +0000

    Add unit test for title and body of Todos

--- backend/todos/tests.py
+++ backend/todos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+class TodoModelTest(TestCase):
+    def setup_test_data(self):
+        return Todo.objects.create(title='todo title', body='todo body')
+
+    def test_title(self):
+        self.setup_test_data()
+        todo = Todo.objects.get(pk=1)
+        self.assertEquals(todo.title, 'todo title')
+
+    def test_body(self):
+        self.setup_test_data()
+        todo = Todo.objects.get(pk=1)
+        self.assertEquals(todo.body, 'todo body')
